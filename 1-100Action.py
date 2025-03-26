cb500a53e4260f7e8161e3ae10f01cbb4199f4f8
shanehaku <shane0922.ee10@nycu.edu.tw>
2025-03-26 20:36:37 +0800

    SDK for realsense

---
+++ 1-100Action.py
@@ -0,0 +1,24 @@
+import socket
+
+def is_valid_action(action):
+    # 检查输入是否是数字且在1到100之间
+    if action.isdigit():
+        number = int(action)
+        return 1 <= number <= 100
+    return False
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_address1 = ("192.168.12.1", 8888)
+
+while True:
+    action = input("请输入要发送的动作（1-100之间的数字）：")
+    
+    if not is_valid_action(action):
+        print("动作类型错误，请输入1到100之间的数字。")
+        print("-----------------------------")
+        continue
+    
+    msg = f'{{"arm_mode":{action}}}'
+    print(f'发送消息: {msg} 到地址: {server_address1}')
+
+    client_socket.sendto(msg.encode(), server_address1)
